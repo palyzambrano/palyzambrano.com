5bc1a6b4036c9c6e9a2a19432352ddbec90e7859
Paulina Zambrano <palyzambrano@gmail.com>
2023-01-11 16:17:33 -0400

    arregla icon card

--- src/routes/blog/components/BlogPost.svelte
+++ src/routes/blog/components/BlogPost.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
+	import Arrow from '~icons/custom/arrow';
+
 	export let post: PalyzambranoDotCom.BlogPost;
 
 	let date = new Date(post.date);
 </script>
-
-<article
-	class="m-4 flex flex-col w-[380px] h-[380px] text-center bg-rosita text-rojito justify-between"
->
-	<span class="text-sm mt-6">{date.getMonth()} {date.getFullYear()}</span>
-	<h3 class="text-4xl w-[340px] m-auto font-extralight">
-		{post.title}
-	</h3>
-	<a class="mb-16" href="/blog/{post.slug}"> ver más -> </a>
-</article>
+<a href="/blog/{post.slug}">
+	<article
+		class="m-4 flex flex-col w-[380px] h-[380px] text-center bg-rosita text-rojito justify-between"
+	>
+		<span class="text-sm mt-6">{date.getMonth()} {date.getFullYear()}</span>
+		<h3 class="text-4xl w-[340px] m-auto font-extralight">
+			{post.title}
+		</h3>
+		<div class="flex justify-center mb-16 ">
+			<p class="text-lg font-extralight"> ver más</p>
+			<Arrow class="w-12 ml-4 fill-rojito"/>
+		</div>
+	</article>
+</a>
--- src/routes/proyectos/components/ProjectEntry.svelte
+++ src/routes/proyectos/components/ProjectEntry.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Arrow from '~icons/custom/arrow';
+
 	export let item: PalyzambranoDotCom.ProjectItem;
 
 	let date = new Date(item.date);
@@ -8,10 +10,13 @@
 	<article
 		class="flex flex-col w-[380px] h-[380px] text-center bg-violet text-white justify-between"
 	>
-		<span class="text-sm mt-6">{date.getMonth()} {date.getFullYear()}</span>
-		<h3 class="text-4xl w-[340px] m-auto font-extralight">
+		<span class="text-sm mt-6 font-normal">{date.getMonth()} {date.getFullYear()}</span>
+		<h3 class="text-4xl w-[340px] m-auto font-light">
 			{item.title}
 		</h3>
-		<span class="mb-16"> ver más -> </span>
+		<div class="flex justify-center mb-16 ">
+			<p class="text-lg font-extralight"> ver más</p>
+			<Arrow class="w-12 ml-4 fill-white"/>
+		</div>
 	</article>
 </a>
