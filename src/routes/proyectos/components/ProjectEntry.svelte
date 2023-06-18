5b501ffc869e00a1909335ccaa0cb39bd4bb3388
Paulina Zambrano <palyzambrano@gmail.com>
2023-06-18 18:33:15 -0400

    button share

--- src/routes/proyectos/components/ProjectEntry.svelte
+++ src/routes/proyectos/components/ProjectEntry.svelte
@@ -3,6 +3,7 @@
 
 	import Button from '$lib/components/Button.svelte';
 	import { humanDate } from '$lib/utils/date';
+	import Share from '$lib/components/Share.svelte';
 
 	export let item: PalyzambranoDotCom.ProjectItem;
 
@@ -13,20 +14,24 @@
 <article
 	class="bg-[url({item.preview_image_url})] flex flex-col w-[380px] h-[480px] text-left justify-between border border-sky-500 rounded-md" 
 >
+	<div class="">
+		<Share> Env</Share>
+		<!-- <a href="/proyectos/{item.slug}">ver más </a> -->
+	</div>
 	<div class="mt-8">
-		<div>
-			<time class="ml-4 mt-8" datetime="{dateString} 00:00">
+		<div class=" ml-6  w-4/5">
+			<time class=" mt-8" datetime="{dateString} 00:00">
 				{humanDate(date)}
 			</time>
-			<h3 class="text-2xl w-[340px] ml-4">
+			<h3 class="text-2xl">
 				{item.title}
 			</h3>
-			<p class="ml-4 w-9/12">{item.description}</p>
+			<p>{item.description}</p>
 		</div>
-		<div>
-			<Button>Paulina</Button>
-			<!-- <a href="/proyectos/{item.slug}">ver más </a> -->
-		</div>
+	</div>
+	<div class=" ml-6 mt-6">
+		<Button>Paulina</Button>
+		<!-- <a href="/proyectos/{item.slug}">ver más </a> -->
 	</div>
 	<div>
 		<figure class="h-[200px] overflow-hidden">
