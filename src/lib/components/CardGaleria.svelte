12eb1f03cfd4ff0338739da2788393a89c3227dd
Paulina Zambrano <palyzambrano@gmail.com>
2025-08-29 00:02:57 +0200

    cardgalery

--- src/lib/components/CardGaleria.svelte
+++ src/lib/components/CardGaleria.svelte
@@ -1,7 +1,7 @@
 <a href="*">
-  <h4 class="uppercase">Colomba</h4>
+  <h4 class="">Colomba</h4>
   <article
-    class="w-80 h-[400px] grayscale hover:grayscale-0 bg-[url(/procesos/colomba/col-procesos.png)] bg-cover flex justify-center items-center"
+    class="w-80 h-[400px] hover:grayscale-0 bg-[url(/procesos/colomba/col-procesos.png)] bg-cover flex justify-center items-center"
   >
     <div
       class="text-white text-center w-full h-full bg-zinc-900/40 flex flex-col justify-center items-center"
