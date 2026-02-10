6b682e02b704d551de33fa60218bdc4a2b4c1fd9
Paly Zambrano <palyzambrano@gmail.com>
2026-02-10 12:56:45 +0100

    42 images roomy (#43)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,20 @@
 <script>
-  let videoSrc = '#';
+  let videoId = 'JrMjRCEAV2Q';
 </script>
 
 <section
   class="w-full flex flex-col md:flex-row justify-center md:items-end md:pt-6"
 >
   <div class="md:w-4/12 p-4">
-    <div>
-      <video
-        src={videoSrc}
-        autoplay
-        loop
-        muted
-        playsinline
-        class="w-full max-w-4xl md:h-[620px] mx-auto shadow-lg object-cover"
-      >
-        Tu navegador no soporta el elemento video.
-      </video>
-    </div>
+    <iframe
+      src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&loop=1&playlist=${videoId}&controls=0`}
+      title="Paly Zambrano"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+      class="w-full h-[482px] md:w-[435px] md:h-[580px] object-cover aspect-video"
+    />
   </div>
+
   <div class="md:w-4/12 p-4">
     <article
       class="  md:p-8 p-4 space-y-2 bg-zinc-100 text-sm shadow-sm shadow-zinc-300"
