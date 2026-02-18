50f5cb183d4bebbc903933eb921a9ae76ffb5598
Paulina Zambrano <palyzambrano@gmail.com>
2026-02-18 16:48:44 +0100

    delete dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,20 +5,10 @@
   import Footer from '$lib/components/Footer.svelte';
 
   import '../app.css';
-
-  onMount(() => {
-    document.documentElement.classList.toggle(
-      'dark',
-      !('theme' in localStorage) &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches
-    );
-  });
 </script>
 
 <Header />
-<main
-  class="text-gray-950 bg-neutral-50 dark:bg-gray-800 dark:text-neutral-50 min-h-[calc(100vh-(80px+40px))]"
->
+<main class="text-gray-950 bg-neutral-50 min-h-[calc(100vh-(80px+40px))]">
   <slot />
 </main>
 <Footer />
